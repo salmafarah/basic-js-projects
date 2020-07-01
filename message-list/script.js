6167a8426ee65d0460a95d393fdebfff1363a2d6
Salma Farah <salmafarah08@gmail.com>
2020-06-30 23:51:55 -0400

    finished

--- message-list/script.js
+++ message-list/script.js
@@ -1,2 +1,17 @@
-const btn = document.querySelector('.btn');
-cont messIn = document.querySelector('')
+const btn = document.querySelector('#btn');
+const messIn = document.querySelector('#messageIn');
+const messOut = document.querySelector('#messageOut');
+
+btn.addEventListener('click', createMessage);
+
+function createMessage(){
+    let text = []; 
+     text.push(messIn.value); 
+    console.log(text)
+    if(messIn.value === ''){
+        alert('Please enter text')
+    } else {
+    messOut.innerHTML = text[text.length-1]; 
+    messIn.value = ''
+    }
+}
