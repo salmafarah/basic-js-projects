46ce94c2efabb18e0ac70895a8cda43b7a55e6e3
salmafarah <salmafarah08@gmail.com>
2020-09-16 18:14:33 -0400

    found hours, minutes, secs, days

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -3,7 +3,14 @@ const Bday = '8 December 2020'
 function countdown(){
  const BdayDate = new Date (Bday);
  const currentDate = new Date();
- console.log( BdayDate - currentDate)
+
+ const totalSeconds = Math.floor(BdayDate - currentDate)/1000;
+ const days = Math.floor(totalSeconds /3600/24);
+ const minutes = Math.floor(totalSeconds /60) % 60;
+ const hours = Math.floor(totalSeconds /3600) % 24;
+ const seconds = Math.floor(totalSeconds) % 60; 
+console.log(days,hours, minutes,seconds)
+
 }
 
 countdown()
