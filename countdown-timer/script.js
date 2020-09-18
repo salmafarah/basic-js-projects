0f8d849d05d5f140c1c303423d04c2c2119dbbc9
salmafarah <salmafarah08@gmail.com>
2020-09-18 18:50:02 -0400

    complete project1

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,3 +1,8 @@
+const daysEl =  document.getElementById("days")
+const hoursEl = document.getElementById("hours")
+const minutesEl = document.getElementById("minutes")
+const secondsEl = document.getElementById("seconds")
+
 const Bday = '8 December 2020'
 
 function countdown(){
@@ -6,11 +11,21 @@ function countdown(){
 
  const totalSeconds = Math.floor(BdayDate - currentDate)/1000;
  const days = Math.floor(totalSeconds /3600/24);
- const minutes = Math.floor(totalSeconds /60) % 60;
+ const minutes = Math.floor(totalSeconds/60) % 60;
  const hours = Math.floor(totalSeconds /3600) % 24;
  const seconds = Math.floor(totalSeconds) % 60; 
-console.log(days,hours, minutes,seconds)
 
+ daysEl.innerHTML = days 
+ hoursEl.innerHTML = hours
+ minutesEl.innerHTML = formatTime(minutes)
+ secondsEl.innerHTML = formatTime(seconds)
+}
+//Calls the function as soon as the page loads 
+countdown();
+
+function formatTime(time){
+ return time < 10 ? (`0${time}`) : time;
 }
 
-countdown()
+
+setInterval(countdown,1000);
