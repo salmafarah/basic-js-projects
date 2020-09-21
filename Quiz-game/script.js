54b4f15419de9cddd6443ef7be1e955212228f93
salmafarah <salmafarah08@gmail.com>
2020-09-21 09:18:06 -0400

    update html and cont. with css

---
+++ Quiz-game/script.js
@@ -0,0 +1,39 @@
+const quizData = [
+    {
+        question: "What is Samia's nickname?",
+        a: "Sam",
+        b: "sam-dam",
+        c: "sam-cat",
+        d:"samsam",
+        correct: "d"
+    }, {
+        question: "What part of her body does Sumaya like? ",
+        a: "face",
+        b: "leg",
+        c: "hair",
+        d:"eyes",
+        correct: "b"
+    },{
+        question: "What does Sumaya want? ",
+        a: "food",
+        b: "new clothes",
+        c: "skirts",
+        d:"shoes",
+        correct: "d"
+    },{
+        question: "What does Samia want from Walmart",
+        a: "make photos",
+        b: "clothes",
+        c: "hair products",
+        d:"to steal",
+        correct: "a"
+    },{
+        question: " What Salma's faviourte show ",
+        a: "Brooklyn 99",
+        b: "IT Crowd",
+        c: "steven universe",
+        d:"Adventure Time",
+        correct: "b"
+    },
+  
+]
