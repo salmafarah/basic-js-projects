6c5f7c1bc885725cbce8d727aacd2c159e9e5f81
salmafarah <salmafarah08@gmail.com>
2020-10-10 14:08:12 -0400

    made functional

---
+++ Todo-list/script.js
@@ -0,0 +1,32 @@
+//Check out specfic todos 
+
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed")
+    // if($(this).css("color") === "rgb(128, 128, 128)"){
+    //     $(this).css({
+    //         color: "black",
+    //         textDecoration: "none"
+    //     })
+    // }
+    // else {
+    //     $(this).css({
+    //         color: "gray",
+    //         textDecoration: "line-through"
+    //      })
+    // }     
+});
+
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove()
+    })
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var todoText = $(this).val();
+        $(this).val("")
+        $("ul").append("<li><span>X</span> " + todoText + "</li>")
+    }
+})
