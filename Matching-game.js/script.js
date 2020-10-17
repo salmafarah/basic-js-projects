8f600840a80927455e16cc9e9e5e28d8f87c829a
salmafarah <salmafarah08@gmail.com>
2020-10-17 13:46:52 -0400

    made game logic

--- Matching-game.js/script.js
+++ Matching-game.js/script.js
@@ -2,23 +2,56 @@ const cards = document.querySelectorAll(".memory-card")
 
 let hasFlippedCard = false;
 let firstCard, secondCard; 
-
+let lockBoard = false; 
 
 
 
 function flipCard(){
+    if (lockBoard) return 
+    if (this === firstCard) return 
+
    this.classList.add("flip")
 
    if(!hasFlippedCard){
        hasFlippedCard = true;
-       firstCard = this 
-
-       console.log(hasFlippedCard, firstCard)
-   }
-
-
+       firstCard = this;
+       return
+   } 
+       secondCard = this;
+       matchingLogic();
 
 }
+
+function matchingLogic(){
+    if(firstCard.dataset.framework === secondCard.dataset.framework){
+        disableCard()
+    } else{
+        unflipCards()
+
+    }
+}
+
+function disableCard(){
+    firstCard.removeEventListener("click", flipCard)
+    secondCard.removeEventListener("click", flipCard)
+    resetBoard()
+}
+
+function unflipCards(){
+    lockBoard = true; 
+    setTimeout(() => {
+        firstCard.classList.remove('flip')
+        secondCard.classList.remove('flip')
+        resetBoard()
+    }, 1500)
+
+}
+
+function resetBoard(){
+    [hasFlippedCard,lockBoard] = [false, false];
+    [firstCard,secondCard]= [null, null];
+}
+
 
 cards.forEach(card => card.addEventListener("click", flipCard))
 
